dd56be71f1595296f283caf5929e377a93c57360
sravanDwadasi <sravandwadasi@gmail.com>
2025-04-10 23:23:27 +0530

    Added my first Angular project

---
+++ src/app/app.component.ts
@@ -0,0 +1,27 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { HeaderComponent } from './components/header/header.component';
+
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet, HeaderComponent],
+  template: `
+  <app-header />
+  <main>
+    <router-outlet />
+  </main>
+  `,
+  styles:[
+    `
+    main {
+      padding: 16px;
+    }
+    `
+  ]
+})
+export class AppComponent {
+  title = 'first-ng-app';
+  
+}
---
+++ src/app/pipes/filter-todos.pipe.ts
@@ -0,0 +1,19 @@
+import { Pipe, PipeTransform } from '@angular/core';
+import { Todo } from '../model/todo.type';
+
+@Pipe({
+  name: 'filterTodos'
+})
+export class FilterTodosPipe implements PipeTransform {
+
+  transform(todos: Todo[], searchTerm: String): Todo[] {
+    if(!searchTerm){
+      return todos;
+    }
+    const text = searchTerm.toLowerCase();
+    return todos.filter(todo => {
+      return todo.title.toLocaleLowerCase().includes(text);
+    })
+  }
+
+}
